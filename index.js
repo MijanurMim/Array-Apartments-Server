a77f65e33343a474487f9b23d3a0d9f2a4649b82
MijanurMim <mjanurmim1@gmail.com>
2021-11-10 22:26:21 +0600

    restart the project

--- index.js
+++ index.js
@@ -22,61 +22,8 @@ console.log(uri);
 async function run() {
   try {
     await client.connect();
+    console.log("database connected to array");
     const database = client.db("array_apartments");
-    const apartmentsCollection = database.collection("apartments");
-    const usersCollection = database.collection("users");
-
-    // Users Area
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      console.log(result);
-      res.json(result);
-    });
-
-    app.put("/users", async (req, res) => {
-      const user = req.body;
-      const filter = { email: user.email };
-      const options = { upsert: true };
-      const updateDoc = { $set: user };
-
-      const result = await usersCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.json(result);
-    });
-
-    //  Creating Admin
-    app.put("/users/admin", verifyToken, async (req, res, next) => {
-      const user = req.body;
-      const requester = req.decodedEmail;
-      if (requester) {
-        const requesterAccount = await usersCollection.findOne({
-          email: requester,
-        });
-        if (requesterAccount.role === "admin") {
-          const filter = { email: user.email };
-          const updateDoc = { $set: { role: "admin" } };
-          const result = await usersCollection.updateOne(filter, updateDoc);
-          res.json(result);
-        }
-      } else {
-        res.status(401).json({ message: "You do not have the permisson" });
-      }
-    });
-
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const user = await usersCollection.findOne(query);
-      let isAdmin = false;
-      if (user?.role === "admin") {
-        isAdmin = true;
-      }
-      res.json({ admin: isAdmin });
-    });
   } finally {
     // await client.close();
   }
