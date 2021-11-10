ff02b57389a740c7a472d567b8077f2ef7e142f2
MijanurMim <mjanurmim1@gmail.com>
2021-11-10 20:54:54 +0600

    admin added

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+// MongoDb Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kin6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("array_apartments");
+    const apartmentsCollection = database.collection("apartments");
+    const usersCollection = database.collection("users");
+
+    // Users Area
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    //  Creating Admin
+    app.put("/users/admin", verifyToken, async (req, res, next) => {
+      const user = req.body;
+      const requester = req.decodedEmail;
+      if (requester) {
+        const requesterAccount = await usersCollection.findOne({
+          email: requester,
+        });
+        if (requesterAccount.role === "admin") {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        }
+      } else {
+        res.status(401).json({ message: "You do not have the permisson" });
+      }
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Array Apartments server is running ");
+});
+
+app.listen(port, () => {
+  console.log("Server is Running at port ", port);
+});
