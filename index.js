e771749d78580c55dd07f0978c2e20033bd05b20
MijanurMim <mjanurmim1@gmail.com>
2021-11-11 12:12:10 +0600

    Registration and log in done

--- index.js
+++ index.js
@@ -22,16 +22,147 @@ console.log(uri);
 async function run() {
   try {
     await client.connect();
-    console.log("database connected to array");
+    console.log("database connected ty array ");
     const database = client.db("array_apartments");
+    const apartmentCollection = database.collection("apartments");
+    const bookingCollection = database.collection("bookings");
+    const usersCollection = database.collection("users");
+
+    const membersCollection = database.collection("members");
+
+    // ADD PACKAGES
+    app.post("/addEvent", async (req, res) => {
+      console.log(req.body);
+      const result = await apartmentCollection.insertOne(req.body);
+      console.log(result);
+    });
+
+    // ADD Boookings
+    app.post("/addNewBooking", async (req, res) => {
+      console.log(req.body);
+      const result = await bookingCollection.insertOne(req.body);
+      console.log(result);
+    });
+
+    // ADD Members
+    app.post("/addMember", async (req, res) => {
+      console.log(req.body);
+      const result = await membersCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // Searched Packages
+    app.get("/searchPackages", async (req, res) => {
+      console.log(req.query.search);
+      const result = await apartmentCollection
+        .find({
+          title: { $regex: req.query.search },
+        })
+        .toArray();
+      res.send(result);
+      console.log(result);
+    });
+
+    // Get ALL Members
+    app.get("/allMembers", async (req, res) => {
+      const result = await membersCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Get ALL Packages
+    app.get("/allPackages", async (req, res) => {
+      const result = await apartmentCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // GET all Bookings
+    app.get("/allBookings", async (req, res) => {
+      const result = await bookingCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Delete Package
+    app.delete("/deletePackage/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await apartmentCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+    // Delete Booking
+    app.delete("/deleteBooking/:id", async (req, res) => {
+      const result = await bookingCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    // Single Package Details
+    app.get("/packageDetails/:id", async (req, res) => {
+      // console.log(req.params.id);
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await apartmentCollection.findOne(query);
+      res.json(result);
+    });
+
+    // My Bookings
+    app.get("/myBookings/:email", async (req, res) => {
+      // console.log(req.params);
+      const result = await bookingCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+      // console.log(result);
+    });
+    // My Booking details
+    app.post("/myBookings/:email", async (req, res) => {
+      const result = await bookingCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+      // console.log(result);
+    });
+
+    // Update status
+    app.put("/updateStatus/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await bookingCollection.updateOne(
+        { _id: ObjectId(req.params.id) },
+        { $set: { status: "Approved" } }
+      );
+      res.send(result);
+    });
+
+    // Users area
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
   } finally {
-    // await client.close();
+    // await client.close()
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Array Apartments server is running ");
+  res.send("Fellow Travellers server is running ");
 });
 
 app.listen(port, () => {
